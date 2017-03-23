137b6118152cba713e7f72cbc47e448ed79c019a
viktor kovylin <korpus99@mail.ru>
2017-03-23 18:49:18 +0300

    initial commit

---
+++ Source/OnTheBottom/OnTheBottom.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OnTheBottom.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OnTheBottom, "OnTheBottom" );
---
+++ Source/OnTheBottom/OnTheBottomGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "OnTheBottomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONTHEBOTTOM_API AOnTheBottomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
